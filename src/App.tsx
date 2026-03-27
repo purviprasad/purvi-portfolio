f81889a42a20e0d0be5c95090c4a1b517e9c7e08
purviprasad <purviprasadhit@gmail.com>
2026-03-27 14:01:05 +0530

    remove hash from urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy, useEffect, useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SnowEffect from "./components/SnowEffect";
 import ThemeProvider from "./components/ThemeProvider";
